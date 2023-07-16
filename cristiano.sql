8c0edbe7b1324bc85c2dbe15e7f72c656a7ffee6
Pablo Skovronski Alves <78922639+pablitosko@users.noreply.github.com>
2023-07-16 13:30:29 -0400

    Update cristiano.sql

--- cristiano.sql
+++ cristiano.sql
@@ -16,10 +16,12 @@
 
 
 -- Copiando estrutura do banco de dados para cristiano
+DROP DATABASE IF EXISTS `cristiano`;
 CREATE DATABASE IF NOT EXISTS `cristiano` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `cristiano`;
 
 -- Copiando estrutura para tabela cristiano.estudante_basico
+DROP TABLE IF EXISTS `estudante_basico`;
 CREATE TABLE IF NOT EXISTS `estudante_basico` (
   `id` mediumint(8) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -42,6 +44,21 @@ INSERT IGNORE INTO `estudante_basico` (`id`, `nome`, `matricula`, `cpf`, `endere
 	(00000006, 'teste', '0000004', '', 'xxxx', '', '', '78300000', 'teste@teste.com'),
 	(00000011, 'teste', '0000005', '', 'xxxx', '', '', '78300000', 'teste@teste.com'),
 	(00000013, 'teste', '0000007', '', 'xxxx', '', '', '78300000', 'teste@teste.com');
+
+-- Copiando estrutura para tabela cristiano.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  UNIQUE KEY `users_id_IDX` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela cristiano.users: ~2 rows (aproximadamente)
+INSERT IGNORE INTO `users` (`id`, `username`, `name`, `password`) VALUES
+	(2, 'pablitosko', 'Pablo Skovronski Alves', 'Sk0vr0n5k!'),
+	(3, 'cristiano', 'cristiano', '5mm@d31r@5');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
